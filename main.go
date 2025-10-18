1b47a2cca45030956a9383192de2a046efd07db8
Moaz Ahmed <justmeyo2025@gmail.com>
2025-10-19 02:48:54 +0300

    finish: Day 1.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// new serve mux router instance
+	mux := http.NewServeMux()
+
+	// handling all routes
+	mux.HandleFunc("/{$}", Home)
+	mux.HandleFunc("/snippet/create", SnippetCreate)
+	mux.HandleFunc("/snippet/view", SnippetView)
+
+	fmt.Println("SnippetBox server started on :4000")
+
+	// start the server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from SnippetBox"))
+}
+
+func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func SnippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
 }

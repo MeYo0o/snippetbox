0ac155f01f2c57b1aa2f033b11a2cc926c98e5dd
Moaz Ahmed <justmeyo2025@gmail.com>
2025-10-20 10:20:28 +0300

    finish: Day 2.

--- main.go
+++ main.go
@@ -11,25 +11,11 @@ func main() {
 	mux := http.NewServeMux()
 
 	// handling all routes
-	mux.HandleFunc("/{$}", Home)
-	mux.HandleFunc("/snippet/create", SnippetCreate)
-	mux.HandleFunc("/snippet/view", SnippetView)
+	handleRoutes(mux)
 
 	fmt.Println("SnippetBox server started on :4000")
 
 	// start the server
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-}
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello from SnippetBox"))
-}
-
-func SnippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
-}
-
-func SnippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
 }

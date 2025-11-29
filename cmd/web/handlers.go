d89c4061089a6d9b130a27c4e4643263afe39a27
Moaz Ahmed <justmeyo2025@gmail.com>
2025-11-29 19:21:01 +0200

    finish: Day 12.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -64,7 +65,26 @@ func snippetView(app *config.Application) http.HandlerFunc {
 			return
 		}
 
-		fmt.Fprintf(w, "%+v", snippet)
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			"./ui/html/pages/view.tmpl",
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			serverError(app, w, r, err)
+			return
+		}
+
+		data := templateData{
+			Snippet: snippet,
+		}
+
+		err = ts.ExecuteTemplate(w, "base", data)
+		if err != nil {
+			serverError(app, w, r, err)
+		}
 	}
 }
 
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/MeYo0o/snippetbox/internal/models"
+
+type templateData struct {
+	Snippet models.Snippet
+}

39cd5ff7b2a2a17c5df4250d60a73f3854c839b7
Moaz Ahmed <justmeyo2025@gmail.com>
2026-07-09 21:38:33 +0300

    feat: implement user signup and login functionality with routing updates

--- components/partials_templ.go
+++ components/partials_templ.go
@@ -29,7 +29,7 @@ func Nav() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav><a href=\"/\">Home</a> <a href=\"/snippet/create\">Create snippet</a></nav>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav><div><a href=\"/\">Home</a> <a href=\"/snippet/create\">Create snippet</a></div><div><a href=\"/user/signup\">Signup</a> <a href=\"/user/login\">Login</a><form action=\"/user/logout\" method=\"post\"><button>Logout</button></form></div></nav>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
